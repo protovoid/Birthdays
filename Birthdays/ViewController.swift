c215980f355b63e026f21480a82caa65f6919343
Chad Williams <fadewave@gmail.com>
2016-07-29 20:47:07 -0700

    added contact refetching

--- Birthdays/ViewController.swift
+++ Birthdays/ViewController.swift
@@ -11,24 +11,24 @@ import UIKit
 import Contacts
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, AddContactViewControllerDelegate {
-
-    @IBOutlet weak var tblContacts: UITableView!
   
-    var contacts = [CNContact]()
+  @IBOutlet weak var tblContacts: UITableView!
+  
+  var contacts = [CNContact]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        navigationController?.navigationBar.tintColor = UIColor(red: 241.0/255.0, green: 107.0/255.0, blue: 38.0/255.0, alpha: 1.0)
-        
-        configureTableView()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    navigationController?.navigationBar.tintColor = UIColor(red: 241.0/255.0, green: 107.0/255.0, blue: 38.0/255.0, alpha: 1.0)
+    
+    configureTableView()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
   
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     if let identifier = segue.identifier {
@@ -38,44 +38,70 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
       }
     }
   }
-
-    
-    // MARK: IBAction functions
-    
-    @IBAction func addContact(sender: AnyObject) {
-        performSegueWithIdentifier("idSegueAddContact", sender: self)
+  
+  
+  // MARK: IBAction functions
+  
+  @IBAction func addContact(sender: AnyObject) {
+    performSegueWithIdentifier("idSegueAddContact", sender: self)
+  }
+  
+  
+  // MARK: Custom functions
+  
+  func configureTableView() {
+    tblContacts.delegate = self
+    tblContacts.dataSource = self
+    tblContacts.registerNib(UINib(nibName: "ContactBirthdayCell", bundle: nil), forCellReuseIdentifier: "idCellContactBirthday")
+  }
+  
+  
+  func refetchContact(contact contact: CNContact, atIndexPath indexPath: NSIndexPath) {
+    AppDelegate.getAppDelegate().requestForAccess { (accessGranted) -> Void in
+      if accessGranted {
+        let keys = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactEmailAddressesKey, CNContactBirthdayKey, CNContactImageDataKey]
+        
+        do {
+          let contactRefetched = try AppDelegate.getAppDelegate().contactStore.unifiedContactWithIdentifier(contact.identifier, keysToFetch: keys)
+          self.contacts[indexPath.row] = contactRefetched
+          
+          dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            self.tblContacts.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+          })
+        }
+        catch {
+          print("Unable to refetch the contact: \(contact)", separator: "", terminator: "\n")
+        }
+      }
     }
+  }
+  
+  
+  
+  // MARK: UITableView Delegate and Datasource functions
+  
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return contacts.count
+  }
+  
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    //let cell = UITableViewCell()
+    let cell = tableView.dequeueReusableCellWithIdentifier("idCellContactBirthday") as! ContactBirthdayCell
     
+    let currentContact = contacts[indexPath.row]
     
-    // MARK: Custom functions
+    cell.lblFullname.text = "\(currentContact.givenName) \(currentContact.familyName)"
     
-    func configureTableView() {
-        tblContacts.delegate = self
-        tblContacts.dataSource = self
-        tblContacts.registerNib(UINib(nibName: "ContactBirthdayCell", bundle: nil), forCellReuseIdentifier: "idCellContactBirthday")
+    if !currentContact.isKeyAvailable(CNContactBirthdayKey) || !currentContact.isKeyAvailable(CNContactImageDataKey) || !currentContact.isKeyAvailable(CNContactEmailAddressesKey) {
+      refetchContact(contact: currentContact, atIndexPath: indexPath)
     }
-    
-    
-    // MARK: UITableView Delegate and Datasource functions
-    
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contacts.count
-    }
-    
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        //let cell = UITableViewCell()
-        let cell = tableView.dequeueReusableCellWithIdentifier("idCellContactBirthday") as! ContactBirthdayCell
-      
-        let currentContact = contacts[indexPath.row]
-      
-      cell.lblFullname.text = "\(currentContact.givenName) \(currentContact.familyName)"
-      
+    else {
       
       // set the birthday info.
       if let birthday = currentContact.birthday {
@@ -107,15 +133,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
       else {
         cell.lblEmail.text = "Email not available"
       }
-        
-        return cell
-        
+      
     }
-    
-    
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 100.0
-    }
+    return cell
+  }
+  
+  
+  
+  func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    return 100.0
+  }
   
   func didFetchContacts(contacts: [CNContact]) {
     for contact in contacts {
@@ -123,6 +150,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     tblContacts.reloadData()
   }
-
+  
 }
 
